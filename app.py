5e420a5428ce8a76bedbfa024b50c6809c46cb46
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-06-10 20:13:11 +0530

    ⚒ refactor- added class methods
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- app.py
+++ app.py
@@ -20,15 +20,61 @@ parser=argparse.ArgumentParser(
 args=parser.parse_args()
 
 
-def signup(email, pwd, conf_pwd):
-    if email is None and pwd is None and conf_pwd is None:
-        email = input("Enter email address: ")
-        pwd = getpass.getpass(prompt="Enter password: ")
-        conf_pwd = getpass.getpass(prompt="Re-Enter password: ")
 
-    if conf_pwd == pwd:
+class User:
+    
+    logged_in = False
+
+    def __init__(self, email, pwd):
+        self.email =  email
+        self.pwd = pwd
+
+    # Method to login the user
+    def login(self):
+
+        # Hash the enterred password
+        auth = self.pwd.encode()
+        auth_hash = hashlib.md5(auth).hexdigest()
+        # Check if the enterred details are correct
+        with open("credentials.txt", "r") as f:
+            for line in f:
+                stored_email, stored_pwd = line.split(":")
+                parsed_pwd = stored_pwd.split('\n')[0]
+
+                if self.email == stored_email and auth_hash == parsed_pwd:
+                    self.logged_in = True
+                    print("Logged In Successfully!")
+                    return True
+            print("Login failed! \n")
+            return False
+
+    # Method to logut the user
+    def logout(self):
+        self.logged_in = False
+        print('Logout Successfull!')
+        return True
+
+    # Method to verify the login credentials of a user
+    def authenticate(self):
+
+        # Hash the enterred password
+        auth = self.pwd.encode()
+        auth_hash = hashlib.md5(auth).hexdigest()
+        # Check if the enterred details are correct
+        with open("credentials.txt", "r") as f:
+            for line in f:
+                stored_email, stored_pwd = line.split(":")
+                parsed_pwd = stored_pwd.split('\n')[0]
+
+                if self.email == stored_email and auth_hash == parsed_pwd:
+                    return True
+            return False
+
+
+    # Equivalent to User Sign Up
+    def save(self):
         # Encode password from string to byte format for hashing
-        enc_pwd = conf_pwd.encode()
+        enc_pwd = self.pwd.encode()
         # Hash the above password
         hash = hashlib.md5(enc_pwd).hexdigest()
 
@@ -36,158 +82,89 @@ def signup(email, pwd, conf_pwd):
             # Go to starting of file and check if email already exists in the file
             f.seek(0)
             content = f.read()
-
-            if email in content:
+            if self.email in content:
                 print('Account already exists with this E-Mail')
                 return 406
-
             # Go to EOF and add the user data to file
             f.seek(2)
-            f.write(email + ':')
+            f.write(self.email + ':')
             f.write(hash + '\n')
         print("You have registered successfully!")
-        return 200
-    else:
-        print("Password is not the same!")
-        return 409
 
+    # Method to update user's password
+    def update_user(self, new_pwd):
+        with open("credentials.txt", "r") as f:
+            replacement = ""
+
+            for line in f:
+                stored_email, stored_pwd = line.split(":")
+                parsed_pwd = stored_pwd.split('\n')[0]
+
+                if self.email == stored_email:
+                    # Get the new password and hash it
+                    new_auth = new_pwd.encode()
+                    new_auth_hash = hashlib.md5(new_auth).hexdigest()
+                    # Replace the old password hash with the new password hash
+                    line = line.replace(parsed_pwd, new_auth_hash)
+
+                replacement = replacement + line
+            
+        # Re-write the file with the new changes
+        with open("credentials.txt", "w") as f:
+            f.write(replacement)
+            print('Password Updated!')
+            return 200
+
+    # Method to delete the user's account
+    def delete_user(self):
+
+        with open("credentials.txt", "r") as f:
+            lines = f.readlines()
+        with open("credentials.txt", "w") as f:
+            for line in lines:
+                if self.email not in line.strip("\n"):
+                    f.write(line)
+                else:
+                    self.logged_in = False
+                    print('Account deleted successfully!')
+
+    # Method to display all the user's
+    def read_users(self):
+        users = []
     
-def login(email, pwd):
-    if email is None and pwd is None: 
-        email = input("Enter email: ")
-        pwd = getpass.getpass(prompt="Enter password: ")
-    
-    # Encode password and then hash it.
-    auth = pwd.encode()
-    auth_hash = hashlib.md5(auth).hexdigest()
-
-    with open("credentials.txt", "r") as f:
-        for line in f:
-            stored_email, stored_pwd = line.split(":")
-            parsed_pwd = stored_pwd.split('\n')[0]
-
-            if email == stored_email and auth_hash == parsed_pwd:
-                global LOGGED_IN
-                LOGGED_IN = True
-                print("Logged In Successfully!")
-                return 200
-
-        print("Login failed! \n")
-        return 403
-
-
-def logout():
-    global LOGGED_IN
-    LOGGED_IN = False
-    print('Logout Completed')
-
-
-# To update a user's password
-def update_user(email, pwd, new_pwd):
-    
-    if email is None and pwd is None:
-        email = input("Enter user's email to update their password: ")
-        pwd = getpass.getpass(prompt="Enter Old Password: ")
-    # Encode password and then hash it.
-    auth = pwd.encode()
-    auth_hash = hashlib.md5(auth).hexdigest()
-
-    
-    with open("credentials.txt", "r") as f:
-        replacement = ""
-        valid_credentials = False
-
-        for line in f:
-            stored_email, stored_pwd = line.split(":")
-            parsed_pwd = stored_pwd.split('\n')[0]
-
-            if email == stored_email and auth_hash == parsed_pwd:
-                valid_credentials = True
-                # Get the new password and hash it
-                if new_pwd is None:
-                    new_pwd = getpass.getpass(prompt="Enter New Password: ")
-                new_auth = new_pwd.encode()
-                new_auth_hash = hashlib.md5(new_auth).hexdigest()
-                # Replace the old password hash with the new password hash
-                line = line.replace(parsed_pwd, new_auth_hash)
-
-            replacement = replacement + line
+        with open("credentials.txt", "r") as f:
+            for line in f:
+                stored_email = line.split(":")[0]
+                users.append(stored_email)
         
-        if valid_credentials == False:
-            print("Incorrect Password! \n")
-            return 403
+        print('List of all users: \n')
+        for user in users:
+            print(user)
 
-    # Re-write the file with the new changes
-    with open("credentials.txt", "w") as f:
-        f.write(replacement)
-        print('Password Updated!')
-        return 200
+        return user
 
-
-# To delete a user's account
-def delete_user(email):
-
-    user_found = False
-    if email is None:
-        email = input("Enter user's email whose account is to be deleted: ")
-
-    with open("credentials.txt", "r") as f:
-        lines = f.readlines()
-    with open("credentials.txt", "w") as f:
-        for line in lines:
-            if email not in line.strip("\n"):
-                f.write(line)
-            else:
-                user_found = True
     
-    if user_found == True:
-        print("User:'",email,"'deleted successfully !")
-        return 200
-    else:
-        print('No account found with that email !')
-        return 404
+    def weather_info(self, lat, lon):
+        API_key = config('API_key')
+        
+        response = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon + "&exclude=minutely,hourly,daily,alerts&units=metric" + "&appid=" + API_key)
+        if response.status_code != 200:
+            return print('Error getting a response from the API')
+        data = response.json()
+        print('Weather at', lat, ',', lon, 'is: ')
+        print('Humidity: ', data['current']['humidity'], "%")
+        print('Pressure: ', data['current']['pressure'], "hPa")
+        print('Temperature: ', data['current']['temp'], "°C")
+        print('Wind Speed: ', data['current']['wind_speed'], "metre/sec")
+        print('Wind Degree: ', data['current']['wind_deg'], "°")
+        print('UV Index: ', data['current']['uvi'])
 
 
-# To display all the user's
-def read_users():
-    users = []
-    
-    with open("credentials.txt", "r") as f:
-        for line in f:
-            stored_email = line.split(":")[0]
-            users.append(stored_email)
-    
-    print('List of all users: \n')
-    for user in users:
-        print(user)
-
-    return user
-
-
-def weather_info():
-    lat = input('Enter latitude of place: ')
-    lon = input('Enter lomgitude of place: ')
-    API_key = config('API_key')
-    
-    response = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon + "&exclude=minutely,hourly,daily,alerts&units=metric" + "&appid=" + API_key)
-    if response.status_code != 200:
-        return print('Error getting a response from the API')
-    data = response.json()
-    print('Humidity: ', data['current']['humidity'], "%")
-    print('Pressure: ', data['current']['pressure'], "hPa")
-    print('Temperature: ', data['current']['temp'], "°C")
-    print('Wind Speed: ', data['current']['wind_speed'], "metre/sec")
-    print('Wind Degree: ', data['current']['wind_deg'], "°")
-    print('UV Index: ', data['current']['uvi'])
 
 
 def main():
-    # Global variable which decides if user is logged in or not
-    global LOGGED_IN
-    LOGGED_IN = False
-    global USER_MANAGEMENT
     USER_MANAGEMENT = False
-    global WEATHER_INFO
+    LOGGED_IN = False
     WEATHER_INFO = False
 
     # Menu-driven program
@@ -198,34 +175,69 @@ def main():
             print("2. Login")
             print("3. Exit")
             ch = int(input("Enter your choice: "))
+            
             if ch == 1:
-                signup(None, None, None)
+                email = input('Enter your email: ')
+                pwd = getpass.getpass('Enter your password: ')
+                conf_pwd = getpass.getpass('Enter password again: ')
+                if pwd == conf_pwd:
+                    user = User(email, pwd)
+                    user.save()
+                else:
+                    print('Password is not the same')
+
             elif ch == 2:
-                login(None, None)
+                email = input('Enter email: ')
+                pwd = getpass.getpass('Enter password: ')
+                user = User(email, pwd)
+                login = user.login()
+                if login:
+                    LOGGED_IN = True
+
             elif ch == 3:
                 break
+
             else:
                 print("Wrong Choice!")
         else:
             if USER_MANAGEMENT == True:
                 print("----------- USER MANAGEMENT ----------------")
-                print("1. Create a new user")
-                print("2. Update a user's password")
-                print("3. Delete an existing user")
-                print("4. Display all the user's")
-                print("5. Go back to dashboard")
-                    
+                print("1. Add a new user")
+                print("2. Update my password")
+                print("3. Delete my account")
+                print("4. Display all the users")
+                print("5. Go back to dashboard")    
                 ch = int(input("Enter your choice: "))
+
                 if ch == 1:
-                    signup(None, None, None)
+                    email = input('Enter email: ')
+                    pwd = getpass.getpass('Enter password: ')
+                    conf_pwd = getpass.getpass('Enter password again: ')
+                    if pwd == conf_pwd:
+                        add_user = User(email, pwd)
+                        add_user.save()
+                    else:
+                        print('Password is not the same')
+
                 elif ch == 2:
-                    update_user(None, None, None)
+                    pwd = getpass.getpass(prompt="Enter Old Password: ")
+                    user = User(user.email, pwd)
+                    if user.authenticate():
+                        new_pwd = getpass.getpass(prompt="Enter New Password: ")
+                        user.update_user(new_pwd)
+
                 elif ch == 3:
-                    delete_user(None)
+                    confirmation = input('Are you sure you want to delete your account? (y/n)')
+                    if confirmation == 'y' or confirmation == 'Y':
+                        user.delete_user()
+                        LOGGED_IN = False
+
                 elif ch == 4:
-                    read_users()
+                    user.read_users()
+
                 elif ch == 5:
                     USER_MANAGEMENT = False
+
                 else:
                     print("Wrong Choice!")
 
@@ -234,27 +246,34 @@ def main():
                 print("1. Get weather information about a place")
                 print("2. Go back to main menu")
                 ch = int(input("Enter your choice: "))
+
                 if ch == 1:
-                    weather_info()
+                    lat = input('Enter latitude of place: ')
+                    lon = input('Enter longitude of place: ')
+                    user.weather_info(lat, lon)
+
                 elif ch == 2:
                     WEATHER_INFO = False
                 else:
+
                     print("Wrong Choice!")
-            
-            print("----------- WELCOME TO USER DASHBOARD ----------------")
-            print("1. User Management")
-            print("2. Weather Info")
-            print("3. Logout")
-            
-            ch = int(input("Enter your choice: "))
-            if ch == 1:
-                USER_MANAGEMENT = True
-            elif ch == 2:
-                WEATHER_INFO = True
-            elif ch == 3:
-                logout()
-            else:
-                print("Wrong Choice!")
+
+            else: 
+                print("----------- WELCOME TO USER DASHBOARD ----------------")
+                print("1. User Management")
+                print("2. Weather Info")
+                print("3. Logout")
+
+                ch = int(input("Enter your choice: "))
+                if ch == 1:
+                    USER_MANAGEMENT = True
+                elif ch == 2:
+                    WEATHER_INFO = True
+                elif ch == 3:
+                    user = user.logged_in
+                    LOGGED_IN = False
+                else:
+                    print("Wrong Choice!")
 
 
 if __name__ == '__main__':
